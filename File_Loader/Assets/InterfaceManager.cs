df6bbe4821271ae14990a9aa078fe10cd3cc81a0
austinirvine <aa1irvine@gmail.com>
2018-11-12 14:28:22 -0600

    Adding sound sample input and recording to Unity

---
+++ File_Loader/Assets/InterfaceManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InterfaceManager : MonoBehaviour {
+
+	//#region Private Variables & Properties
+
+	//#endregion
+
+	//#region Public Variables & Properties
+	public GameObject SubAudioMenu;
+	public bool SubAudioMenuOpen = false;
+	//#endregion
+
+	//#region Public Custom Button Calls
+	void Awake(){
+
+	}
+
+	public void AudioMenu() {
+		if(SubAudioMenuOpen == false) {
+			SubAudioMenu.SetActive(true);
+			SubAudioMenuOpen = true;
+			Debug.Log("We Instantiated onece");
+			RetrieveAudioList();
+		} else if(SubAudioMenu == true){
+			Debug.Log("bullshit");
+		}
+	}
+
+	public void ExitSubMenu() {
+		if(SubAudioMenuOpen == true && SubAudioMenu.activeSelf) {
+			SubAudioMenu.SetActive(false);
+			SubAudioMenuOpen = false;
+		}
+	}
+
+	public void SelectAudio() {
+		Debug.Log("Select Audio");
+	}
+
+	public void ImportAudio() {
+		Debug.Log("Import Audio");
+	}
+
+	public void RecordAudio() {
+		Debug.Log("Record Audio");
+	}
+
+	private void DisplayAudioList(List<string> audio_array) {
+		Debug.Log("Audio Array: "+ audio_array);
+	}
+
+	private void RetrieveAudioList() {
+		List<string> audio_array = new List<string>();
+		audio_array.Add("Cat");
+		audio_array.Add("Mouse");
+		audio_array.Add("Frog");
+		//reads audio folder
+		//check current filters and place in audio_array
+		DisplayAudioList(audio_array);
+	}
+
+	public void AdjustFilters() {
+		//check filter name
+		//then do your thing
+		RetrieveAudioList();
+		return;
+	}
+	//#endregion
+}
